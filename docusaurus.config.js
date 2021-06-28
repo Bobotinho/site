17ab8bf1ffb1491c69a34d5ff735a2f3f88343b8
leandcesar <ccleandroc@gmail.com>
2021-06-28 00:36:38 -0300

    Revert "💰 Add Google AdSense"
    This reverts commit 3b8a08dba005d5e91964781870bc981a337a4991.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,7 @@ module.exports = {
   url: 'https://bobotinho.herokuapp.com',
   baseUrl: '/',
   clientModules: [require.resolve('./snackPlayerInitializer.js')],
-  scripts: [
-    {
-      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
-      "data-ad-client": "ca-pub-9086444938175463",
-      async: true
-    }
-  ],
+  scripts: [],
   favicon: 'img/favicon.ico',
   titleDelimiter: '|',
   customFields: {
